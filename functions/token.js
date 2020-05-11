ff2fdc5814eca8ebbc5224f9233591f02260d116
Phil Nash <philnash@gmail.com>
2020-05-11 18:01:01 +1000

    Create the room using the REST API.

--- functions/token.js
+++ functions/token.js
@@ -12,6 +12,26 @@ exports.handler = async function (context, event, callback) {
   const ROOM = "hello";
 
   // Fetch or create a room
+  // Start by creating an API client. In Twilio Functions you can get an
+  // authorised client from the context.
+  const client = context.getTwilioClient();
+  let room;
+  try {
+    // Fetch the room by name from the API.
+    room = await client.video.rooms(ROOM).fetch();
+  } catch (error) {
+    // If the room can't be found, create a new room.
+    try {
+      room = await client.video.rooms.create({
+        uniqueName: ROOM,
+        type: "group",
+      });
+    } catch (error) {
+      // If this fails, I'm not sure what went wrong!
+      console.error(error);
+      callback(error);
+    }
+  }
 
   // Create an access token
 
